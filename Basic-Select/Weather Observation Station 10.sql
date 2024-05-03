c38c67b5a29b00842542807ae9681cca7fb24afb
vijayasreechoudary <159934602+vijayasreechoudary@users.noreply.github.com>
2024-05-02 22:02:16 -0400

    Update Weather Observation Station 10.sql

--- Basic-Select/Weather Observation Station 10.sql
+++ Basic-Select/Weather Observation Station 10.sql
@@ -1,3 +1,5 @@
 /*Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.*/
 
 select distinct city from station where not regexp_like(city,'[aeiou]$','i');
+ or
+select distinct city from station where city REGEXP '^[aeiouAEIOU]';  /** this works only for MYSQL**/
