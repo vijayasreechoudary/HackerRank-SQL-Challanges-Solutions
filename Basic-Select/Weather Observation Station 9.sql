fbbffb035eb73dc3711bb2bc75904217645bc54c
vijayasreechoudary <159934602+vijayasreechoudary@users.noreply.github.com>
2024-05-02 21:26:56 -0400

    Create Weather Observation Station 9.sql

---
+++ Basic-Select/Weather Observation Station 9.sql
@@ -0,0 +1,4 @@
+/*Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.*/
+
+
+select distinct city from station where regexp_like(city,'^![aeiou]','i')
