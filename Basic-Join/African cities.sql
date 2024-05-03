83cc3e7e3447f1872d4c0f9cd1128153e69e4412
vijayasreechoudary <159934602+vijayasreechoudary@users.noreply.github.com>
2024-05-03 08:33:05 -0400

    Update African cities.sql

--- Basic-Join/African cities.sql
+++ Basic-Join/African cities.sql
@@ -1,1 +1,6 @@
+/* Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.*/
 
+select c.name from city c, country ct
+where c.countrycode=ct.code
+and ct.continent='Africa'
