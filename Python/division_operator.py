b2383635d471ef65590219c60d05077a17432c6b
vijayasreechoudary <159934602+vijayasreechoudary@users.noreply.github.com>
2024-05-04 00:08:11 -0400

    Create division_operator.py

---
+++ Python/division_operator.py
@@ -0,0 +1,13 @@
+/*
+The provided code stub reads two integers,  and , from STDIN.
+
+Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  / .
+
+No rounding or formatting is necessary. */
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(int(a/b))
+    print(a/b)
